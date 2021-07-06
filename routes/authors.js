039856094efe87b2b37652e7308f6a61455a291b
Guna <mg2kofl@gmail.com>
2021-07-06 14:14:52 +0530

    ade some changes

--- routes/authors.js
+++ routes/authors.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
 const Book = require('../models/book')
-
+//
 // All Authors Route
 router.get('/', async (req, res) => {
   let searchOptions = {}
